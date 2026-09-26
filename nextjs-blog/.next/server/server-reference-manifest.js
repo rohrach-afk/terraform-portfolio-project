7dad9b2575ff5c2d69ce819b7b2e660690d22772
Rohrach <lightningcynder@gmail.com>
2026-09-26 15:58:14 -0500

    changed folders

--- nextjs-blog/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CRVO/jcmCuHIW++kruW2faM7jd2vyuybxDxG7tDPWC4=\"\n}"
--- nextjs-blog/out/_next/static/chunks/42l061x0oo_sz.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/3_gaqddt7wora.js",
-  "static/chunks/2afhd-m5mq7o0.js",
-  "static/chunks/turbopack-1_0dm5rih8zn3.js"
-])
